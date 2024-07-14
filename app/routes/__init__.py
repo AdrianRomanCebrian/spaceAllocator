f759ecb4dcf152f36132be91831f2c42bfc744a0
Adrianroce <50020891+Adrianroce@users.noreply.github.com>
2024-07-14 13:52:33 +0200

    ruta raiz

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,3 +1,4 @@
+from app.routes.index import index
 from app.routes.procesador import procesar_file
 from app.routes.get_tasks_db import get_tasks
 from app.routes.get_results_celery import get_results_task
